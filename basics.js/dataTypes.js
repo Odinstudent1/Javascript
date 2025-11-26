497c0861ef1b335b692a14a1e7a942a19216e39c
Odinstudent1 <parthupmanyuu@gmail.com>
2025-11-26 10:41:37 +0000

    yoo

---
+++ basics.js/dataTypes.js
@@ -0,0 +1,14 @@
+
+let age = 18;
+let name = "Parth"
+let isCoding = true;
+let temp = null;
+let value ;
+//number 
+/* number -> 0 to 53 power  
+bigint -> 
+string -> ""
+boolean -> true or false 
+null ->standalone value (empty)
+undefined -> no value provided 
+*/
