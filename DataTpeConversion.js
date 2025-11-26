8a4fc5cb339eb4a66e3c0d41a933a1ca70cb33db
Odinstudent1 <parthupmanyuu@gmail.com>
2025-11-26 11:40:58 +0000

    okay

---
+++ DataTpeConversion.js
@@ -0,0 +1,39 @@
+//implicit conversions(Automatic by JavaScript ) :
+
+// let sumNum = 5+ "10";  //Number + string -> String    
+
+// console.log(typeof sumNum); 
+
+
+// let num = "5" * 10;  // Number * string -> number 
+
+// console.log(typeof num); 
+
+
+// let boleean = true + 1;    // Number -> 2
+// console.log(typeof boleean);
+// console.log(boleean);    
+
+
+// Explicit Conversions (Manual) :
+
+// let string = "20";       // Converts any datatype into Number by using Number()
+//  lol = Number(string);   
+//  console.log(typeof lol);
+//  console.log(lol)
+
+// let Num = 100;                //Converts any datatype into String using String()
+// let ops = String(Num);
+// console.log(ops);
+// console.log(typeof ops);
+
+// let a = "a";                     //Converts any datatype   into  Boolean using Boolean()
+// let boolean = Boolean(a);
+// console.log(boolean);
+// console.log(typeof boolean);
+ 
+
+// let d = "hello"               //Converts any datatype into object using object ()
+// let date = Object(d);
+// console.log(typeof d);
+// console.log(typeof date);
