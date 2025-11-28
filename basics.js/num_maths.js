076f066f6f05c38ded32e9ad06626ff659852f81
Odinstudent1 <parthupmanyuu@gmail.com>
2025-11-28 18:19:20 +0000

    math and num

---
+++ basics.js/num_maths.js
@@ -0,0 +1,34 @@
+const score = 200;
+// console.log(score)
+
+const newScore = new Number(1000);
+// console.log(newScore);
+
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(2));
+
+const precision =112.8690
+// console.log(precision.toPrecision(3))
+
+const india = 1000000;
+// console.log(india.toLocaleString('en-IN'));
+
+
+
+//--------Maths--------------------
+
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(5.6))
+// console.log(Math.floor(5.3))
+// console.log(Math.ceil(5.1))
+// console.log(Math.min(3,4,5,6,7,8))
+// console.log(Math.max(3,4,5,6,7,8))
+
+console.log(Math.random())
+
+const max =20 ;
+const min =10;
+
+console.log(Math.floor((Math.random() * (max -min +1)) + min ))
