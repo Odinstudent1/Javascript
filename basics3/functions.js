3ede6beeb158c5db7373e7f954aa0e9f822f74ca
Odinstudent1 <parthupmanyuu@gmail.com>
2025-11-30 12:23:08 +0000

    functions

---
+++ basics3/functions.js
@@ -0,0 +1,58 @@
+// function AddTwoNumbers(number1,number2){
+//     console.log(number1 + number2)
+// }
+
+// AddTwoNumbers(10,30)
+
+// function AddTwoNumbers(number1,number2){
+//     return number1 + number2;
+// }
+
+// const Add =AddTwoNumbers(10,30)
+// console.log("Result : ", Add)
+
+// function UserLogin(username){
+//     if(!username){
+//         console.log("Please enter your username : ")
+//         return ;
+//     }
+//     return `${username} just logged in `
+// }
+//   function AddTwoNumbers(number1,number2){
+//     console.log(number1 + number2)
+// }
+
+//   console.log(UserLogin("Parth"))
+
+
+//  function Calculator(val1,val2,...num1){
+//     return num1;
+//  }                                              
+
+//  console.log(Calculator(100,200,300,400,500))
+
+
+
+// const user = { 
+//     username:"parth",
+//     price : 199
+// }
+ 
+//  function Func(anyobject){
+//         console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)  
+//  }
+
+//  Func({                            // object 
+//     username : "Pratyaksh",
+//     price : 299,
+//  })
+
+
+
+//  const Myarr =[100,200,300,400]
+
+//  function array(getArray){
+//     return getArray[1]
+//  }
+
+// console.log(array(Myarr));
