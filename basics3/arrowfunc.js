1612376a65a2073d4c6b03320f0aac2489a00348
Odinstudent1 <parthupmanyuu@gmail.com>
2025-12-01 16:25:36 +0000

    arrowfunc

---
+++ basics3/arrowfunc.js
@@ -0,0 +1,52 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+
+// const soup = () =>{
+//     username ="oark"
+//     console.log(this.username)
+// }
+
+// soup()
+
+
+
+
+// const addTwo = (num1,num2) =>{
+//     return num1 + num2;
+// }
+const addTwo = (num1,num2) =>( num1 + num2)
+
+console.log(addTwo(2,3))
